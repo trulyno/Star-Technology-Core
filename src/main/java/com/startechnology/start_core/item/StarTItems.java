b6083934ee7efc5749cb5feaebf9505c94a7972a
stellaurora <jean1398reborn@gmail.com>
2024-12-22 13:57:33 +1100

    Use GTCEU Components instead of custom tooltip handler.

--- src/main/java/com/startechnology/start_core/item/StarTItems.java
+++ src/main/java/com/startechnology/start_core/item/StarTItems.java
@@ -1,15 +1,28 @@
 package com.startechnology.start_core.item;
 
 import com.gregtechceu.gtceu.api.item.ComponentItem;
+import com.gregtechceu.gtceu.api.item.IComponentItem;
+import com.gregtechceu.gtceu.api.item.component.IItemComponent;
 import com.gregtechceu.gtceu.common.item.DataItemBehavior;
+import com.gregtechceu.gtceu.common.item.TooltipBehavior;
 import com.tterrag.registrate.util.entry.ItemEntry;
+import com.tterrag.registrate.util.nullness.NonNullConsumer;
+
+import net.minecraft.network.chat.Component;
 
 import static com.startechnology.start_core.StarTCore.START_REGISTRATE;
 
 public class StarTItems {
+    public static <T extends IComponentItem> NonNullConsumer<T> attach(IItemComponent components) {
+        return item -> item.attachComponents(components);
+    }
+
     public static final ItemEntry<ComponentItem> TOOL_DATA_DNA_DISK = START_REGISTRATE.item("data_dna_disk", ComponentItem::create)
         .lang("Data DNA Disk")
-        .onRegister((item) -> item.attachComponents(new DataItemBehavior(true)))
+        .onRegister(attach(new DataItemBehavior(true)))
+        .onRegister(attach(new TooltipBehavior(lines -> {
+            lines.add(Component.translatable("item.start_core.data_dna_disk.tooltip"));
+        })))
         .register();
 
     public static void init() {
